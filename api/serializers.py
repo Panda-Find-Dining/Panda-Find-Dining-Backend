660123946496f6b5dff73abd0636437296e80877
Ryan Urbanski <ryanurbanski9@gmail.com>
2022-04-24 18:00:40 -0400

    added serializers

---
+++ api/serializers.py
@@ -0,0 +1,58 @@
+from .models import Restaurant, Meal, User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    '''
+    Serialize Data for the User model
+    '''
+    username = serializers.SlugRelatedField(slug_field='username', read_only='True', source='user')
+
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "username",
+            "friends",
+        )
+
+
+class RestaurantSerializer(serializers.ModelSerializer):
+    '''
+    Serialize Data for the Restaurant model
+    '''
+
+    class Meta:
+        model = Restaurant
+        fields = (
+            'id',
+            'name',
+            'lat',
+            'lon',
+            'formatted_address',
+            'place_id',
+            'hours',
+            'business_statius',
+            'icon',
+            'meal',
+        )        
+
+
+class MealSerializer(serializers.ModelSerializer):
+    '''
+    Serialize Data for the Meal model
+    '''
+
+    class Meta:
+        model = Restaurant
+        fields = (
+            'id',
+            'creator',
+            'created_date',
+            'invitee',
+            'location',
+            'radius',
+            'lat',
+            'lon',
+            'restaurant',
+        )        
