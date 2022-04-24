14785dea75dae532177f121bfe46a0fe9d4378d6
Ryan Urbanski <ryanurbanski9@gmail.com>
2022-04-24 17:44:56 -0400

    added models to admin

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import Meal, Restaurant, User
 
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Meal)
+admin.site.register(Restaurant)
