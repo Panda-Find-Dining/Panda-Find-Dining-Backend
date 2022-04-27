d69d8704f91f273f37ff5a7db9386f4ec2cebbeb
Paul Reece <paulreece2@gmail.com>
2022-04-26 22:07:46 -0400

    fixed model issue

--- api/models.py
+++ api/models.py
@@ -22,7 +22,8 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractUser):
-    friends = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='User', blank=True)
+    friends = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='User', blank=True)
 
     def __str__(self):
         return self.username
@@ -40,22 +41,24 @@ class Restaurant(models.Model):
     hours = models.CharField(max_length=200, blank=True)
     business_status = models.BooleanField(default=True)
     icon = models.URLField(blank=True)
-    meal = models.ForeignKey('Meal', on_delete=models.CASCADE, related_name="meal")
+    meal = models.ForeignKey(
+        'Meal', on_delete=models.CASCADE, related_name="meal")
 
     def __str__(self):
         return self.name
 
 
 class Meal(models.Model):
-    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator")
+    creator = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="creator")
     created_date = models.DateTimeField(auto_now_add=datetime.now)
-    invitee = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='invitee')
-    location = models.CharField(blank=False, max_length=100)
+    invitee = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, blank=True, related_name='invitee')
+    location = models.CharField(blank=True, max_length=100)
     radius = models.IntegerField(blank=True)
     lat = models.FloatField(blank=True, null=True)
     lon = models.FloatField(blank=True, null=True)
     # restaurant = models.ForeignKey(Restaurant, blank=True, null=True, on_delete=models.CASCADE, related_name="restaurant")
-    
 
     def __str__(self):
-        return self.name
+        return self.location
