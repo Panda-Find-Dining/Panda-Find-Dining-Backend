cc54d71aff31f1bb091342b76b664676902e2900
Paul Reece <paulreece2@gmail.com>
2022-05-06 15:22:27 -0400

    testing a bug fix

--- api/views.py
+++ api/views.py
@@ -300,7 +300,7 @@ class RestaurantMatchView(generics.ListAPIView):
         number_diners = meal.invitee.all().count()
         selected_count = meal.all_users_have_selected.count()
 
-        greenzone_queryset = restaurants.filter(yes=2)
+        greenzone_queryset = restaurants.filter(yes=3)
 
         # logic for selecting a restaurant from the GreenZone list ===================================
         # get pk of first matched restaurant
